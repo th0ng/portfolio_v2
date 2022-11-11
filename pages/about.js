d79b106990d8a0b79b4e8e22651a372787be60e6
Thongzxje <thonghoangdanh@gmail.com>
2022-11-11 11:36:59 +0200

    about me

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+export default function about() {
+  return (
+    <div>
+      <p>
+        Hi there. I am an second year Software Engineering student from Tampere
+        University of Applied Sciences. Lately I have been focusing on Fullstack
+        Developing. My experience spans across multiple technologies, frontend
+        with HTML, CSS, Javascript, React, Vue and Python, Node, Express for
+        backend. Here you can see some of my latest <a href="#">projects</a>.
+      </p>
+      <p>
+        Feel free to snoop around on my social media even though there is
+        nothing there haha. For inquiries or a how you doing, contact me, I will
+        be happy to hear from you.
+      </p>
+    </div>
+  );
+}
