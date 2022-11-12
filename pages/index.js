8eaf5fcafda44a8e3cffb4886fa7dddb38c70840
Thongzxje <thonghoangdanh@gmail.com>
2022-11-12 16:00:48 +0200

    projects

--- pages/index.js
+++ pages/index.js
@@ -120,22 +120,9 @@ export default function Home() {
                   <li>Get top songs by genres.</li>
                 </ul>
                 <div className="card-actions justify-end">
-                <div className="badge badge-info gap-2">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-4 h-4 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path></svg>
-                  info
-                </div>
-                <div className="badge badge-success gap-2">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-4 h-4 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path></svg>
-                  success
-                </div>
-                <div className="badge badge-warning gap-2">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-4 h-4 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path></svg>
-                  warning
-                </div>
-                <div className="badge badge-error gap-2">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-4 h-4 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path></svg>
-                  error
-                </div>
+                <div className="badge badge-primary">Reactjs</div>
+                <div className="badge badge-primary">Redux</div>
+                <div className="badge badge-primary">Rapid API</div>
                 </div>
               </div>
           </div>
