6c912699d78b0e8977b07e67e70ef48dd60d0b51
Thongzxje <thonghoangdanh@gmail.com>
2022-11-15 11:30:28 +0200

    footer

--- pages/components/Contact.js
+++ pages/components/Contact.js
@@ -1,13 +1,15 @@
-import {AiTwotoneMail} from "react-icons/ai";
+import { AiTwotoneMail } from "react-icons/ai";
 
 export default function Contactform() {
   return (
-    <div className="mockup-code bg-gray-700 shadow-xl shadow-gray-700 text-primary-content mt-3">
-      <pre data-prefix="$"><code>Contact me.</code></pre>
+    <div className="mockup-code bg-purple-400 text-primary-content my-4">
+      <pre data-prefix="$">
+        <code>Contact me.</code>
+      </pre>
       <pre data-prefix=">"></pre>
       <pre data-prefix=">">thonghoangdanh@gmail.com</pre>
       <pre data-prefix=">">+358 414980030</pre>
       <pre data-prefix=">"></pre>
     </div>
-  )
-};
+  );
+}
