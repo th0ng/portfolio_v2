80cf7eb0ca0766162810ace9446b93256a323860
Thongzxje <thonghoangdanh@gmail.com>
2022-12-30 21:54:20 +0200

    adding assets

---
+++ pages/assets/index.js
@@ -0,0 +1,9 @@
+import javascript from "./javascript.png";
+import react from "./react.png";
+import scene from "./scene.png";
+import mockup from "./mockup.png";
+
+export default {
+  javascript,
+  react,
+};
